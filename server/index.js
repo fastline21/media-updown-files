c430b2808efd49bf810f467289bad80f6c01da68
Joshwa Facistol <fastline2123@gmail.com>
2022-06-08 02:07:07 +0800

    Added server route to client build

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
-const path = require('path')
+const path = require('path');
 
 const server = () => {
 	const app = express();
@@ -16,6 +16,18 @@ const server = () => {
 	app.use(logger('dev'));
 
 	app.use('/api', require('./routes'));
+
+	// Serve static assets in production
+	if (process.env.NODE_ENV === 'production') {
+		// Set static folder
+		app.use(express.static('./../client/build'));
+
+		app.get('*', (req, res) =>
+			res.sendFile(
+				path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+			)
+		);
+	}
 
 	const port = process.env.PORT || 5000;
 	app.listen(port, () => console.log(`Server running on localhost:${port}`));
